b75f9ea6623616d648c58efd1386ef33cd1bc7f2
Fabricio Nogueira <nogsantos@gmail.com>
2018-01-29 17:05:09 -0200

    Setting up the tests with mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var darkSouls = require('./index');
+
+describe('darksouls-names', function() {
+    it('should work', function() {
+        expect(true).to.be.true;
+    });
+});
