69759d58a8ae24c160bc4093d328abfc5bc95fa6
Fabricio Nogueira <nogsantos@gmail.com>
2018-01-30 16:23:52 -0200

    chore: Webpack with umd support
    Adding support to UMD version with webpack build

---
+++ webpack.config.babel.js
@@ -0,0 +1,28 @@
+import {
+    join
+} from "path";
+
+const include = join(__dirname, "src");
+
+export default {
+    entry: "./src/index",
+    output: {
+        path: join(__dirname, "dist"),
+        libraryTarget: "umd",
+        library: "darkSoulsNames",
+    },
+    devtool: "source-map",
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                loader: "babel-loader",
+                include
+            },
+            {
+                test: /\.json$/,
+                loader: "json-loader",
+                include
+            },
+        ]
+    }
+};
